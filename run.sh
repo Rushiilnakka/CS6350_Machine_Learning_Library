51f58b2c33d1350712750887fe03e51ba01603fe
Rushiilnakka <145818617+Rushiilnakka@users.noreply.github.com>
2023-09-22 23:23:46 -0600

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Results for car dataset"
+jupyter "ML.ipynb"
+
+echo "Results for bank dataset - unknown"
+jupyter "Question_3.ipynb"
+
+
+echo "Results for car dataset"
+jupyter "Question_3.ipynb"
